c12ac32aa7aefb312a4758b7bcc10551902a0426
darendal <bware43@gmail.com>
2016-12-07 15:59:12 -0500

    Add project files.

---
+++ src/DailyRunner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailyRunner
+{
+    public class Program
+    {
+        public static DailyChallenge solution = new RackManager1();
+
+        public static void Main(string[] args)
+        {
+            solution.Start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Rack Manager 1/RackManager1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailyRunner
+{
+    public class RackManager1 : DailyChallenge
+    {
+        public RackManager1()
+        {
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
