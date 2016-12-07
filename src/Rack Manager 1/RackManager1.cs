0ff0821027951f7e2430d18db82125f8d8c3bd67
darendal <bware43@gmail.com>
2016-12-07 16:49:20 -0500

    Solve first part of challenge (Determine if word can be made using tiles) https://www.reddit.com/r/dailyprogrammer/comments/5go843/20161205_challenge_294_easy_rack_management_1/

--- src/Rack Manager 1/RackManager1.cs
+++ src/Rack Manager 1/RackManager1.cs
@@ -13,7 +13,65 @@ namespace DailyRunner
 
         public void Start()
         {
-            Console.WriteLine("hi");
+            Console.WriteLine($"scrabble(\"ladilmy\", \"daily\")-> {RackCheck("ladilmy", "daily")}");
+            Console.WriteLine($"scrabble(\"eerriin\", \"eerie\")-> {RackCheck("eerriin", "eerie")}");
+            Console.WriteLine($"scrabble(\"orrpgma\", \"program\")-> {RackCheck("orrpgma", "program")}");
+            Console.WriteLine($"scrabble(\"orppgma\", \"program\")-> {RackCheck("orppgma", "program")}");
+        }
+
+        public bool RackCheck(string tiles, string word)
+        {
+            if(tiles.Equals(word))
+            {
+                return true;
+            }
+
+            if(tiles.Length>=word.Length)
+            {
+                var wordFreq = GetCharacterFrequency(word);
+                var tileFreq = GetCharacterFrequency(tiles);
+
+                foreach(var entry in wordFreq)
+                {
+                    int tileCount = 0;
+
+                    tileFreq.TryGetValue(entry.Key, out tileCount);
+
+                    if( !(tileCount >= entry.Value))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+                   
+        }
+
+        public Dictionary<char, int> GetCharacterFrequency(string word)
+        {
+            var returnable = new Dictionary<char, int>();
+
+            foreach(char c in word)
+            {
+                int currentValue = 0;
+
+                if(returnable.TryGetValue(c, out currentValue))
+                {
+                    returnable[c]++;
+                }
+                else
+                {
+                    returnable.Add(c, 1);
+                }
+
+            }
+            return returnable;
         }
     }
 }
